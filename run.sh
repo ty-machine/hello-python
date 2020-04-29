3808589f84c5bfb58c7eae451f59e25db73b4209
Ty Schreiner <t.schreiner@m1finance.com>
2020-04-29 14:48:59 -0500

    silly mistakes double docker

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker login
 docker build hello-python .
-docker docker tag hello-python m1tschreiner/hello-python:circleci
+docker tag hello-python m1tschreiner/hello-python:circleci
 docker push m1tschreiner/hello-python
